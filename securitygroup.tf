6b9b9cd7e47f578a4a91801c8ad0d91610ac6978
mohdziyad <mhdziyadkp@gmail.com>
2018-03-05 16:26:15 +0530

    sg update

--- securitygroup.tf
+++ securitygroup.tf
@@ -2,19 +2,19 @@ resource "aws_security_group" "new"{
   name = "instance_sg"
   vpc_id = "${aws_vpc.assignment.id}"
   ingress{
-    from_port = "0"
+    from_port = "80"
     to_port = "80"
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress{
-    from_port = "0"
+    from_port = "443"
     to_port = "443"
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress{
-    from_port = "0"
+    from_port = "22"
     to_port = "22"
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
