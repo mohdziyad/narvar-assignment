fda6fdae21ae43dbc0d995925b59890134ac64ca
mohdziyad <mhdziyadkp@gmail.com>
2018-03-05 15:28:27 +0530

    vpc vonfigurations added

--- vpc.tf
+++ vpc.tf
@@ -5,3 +5,10 @@ resource "aws_vpc" "assignment"{
     Name = "assignment-1"
   }
 }
+resource "aws_internet_gateway" "a-igw"{
+  vpc_id = "${aws_vpc.assignment.id}"
+}
+resource "aws_subnet" "a_pub_sub"{
+  vpc_id = "${aws_vpc.assignment.id}"
+  cidr_block = "10.0.1.0/24"
+}
