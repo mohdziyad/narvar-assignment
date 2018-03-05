0ff27bf16aadb0a44f2f78e2e8564f1a89f044fe
mohdziyad <mhdziyadkp@gmail.com>
2018-03-05 16:01:30 +0530

    vpc config to setup route

--- vpc.tf
+++ vpc.tf
@@ -12,8 +12,8 @@ resource "aws_subnet" "a_pub_sub"{
   vpc_id = "${aws_vpc.assignment.id}"
   cidr_block = "10.0.1.0/24"
 }
-resource "aws_route_table" "main"{
-  vpc_id = "${aws_vpc.assignment.id}"
+resource "aws_default_route_table" "default"{
+  vpc_id = "${aws_vpc.assignment.id.default_route_table_id}"
   route{
     cidr_block = "0.0.0.0/0"
     gateway_id = "${aws_internet_gateway.a-igw.id}"
