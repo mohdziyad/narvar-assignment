34adb2852d4befb84b81a3a07b96a865607891bc
mohdziyad <mhdziyadkp@gmail.com>
2018-03-05 17:13:58 +0530

    instance launch

--- instance.tf
+++ instance.tf
@@ -1,8 +1,12 @@
+resource "aws_key_pair" "deploy"{
+  key_name = "web-deploy"
+  public_key = "${file("/home/vagrant/.ssh/id_rsa.pub")}"
+}
 resource "aws_instance" "web"{
   ami = "ami-531a4c3c"
   instance_type = "t2.micro"
-  vpc_id = "${aws_vpc.assignment.id}"
-  key_name = "ziyad_mumbai"
-  vpc_security_group_ids = "${aws_security_group.new.id}"
-  associate_public_ip_address = "yes"
+  subnet_id = "${aws_subnet.a_pub_sub.id}"
+  key_name = "web-deploy"
+  vpc_security_group_ids = ["${aws_security_group.new.id}"]
+  associate_public_ip_address = "true"
 }
