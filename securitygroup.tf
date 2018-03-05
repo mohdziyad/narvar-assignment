5e757f2549feddb1dce1355d15ff5aca1873dce9
mohdziyad <mhdziyadkp@gmail.com>
2018-03-05 19:30:21 +0530

    sg changes

--- securitygroup.tf
+++ securitygroup.tf
@@ -20,4 +20,10 @@ resource "aws_security_group" "new"{
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  egress{
+    from_port = "0"
+    to_port = "0"
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
