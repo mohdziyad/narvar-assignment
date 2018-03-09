6767aff68eefc44e196a9492936e30e3b69a85c7
mohdziyad <mhdziyadkp@gmail.com>
2018-03-09 17:45:41 +0530

    iam role for ec2-cloudwatch

--- terraform-scripts-vpc/cloudwatch-alarms.tf
+++ terraform-scripts-vpc/cloudwatch-alarms.tf
@@ -2,6 +2,23 @@ resource "aws_sns_topic" "alarm_email"{
   name = "cloudwatch_alarm"
 }
 
+resource "aws_iam_role" "cloudwatch_role" {
+    name = "cloudwatch_role_ec2"
+    assume_role_policy = <<EOF
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Sid" : "cloudwatc-ec2"
+          "Action" : "cloudwatch:*"
+          "Effect" : "Allow"
+          "Resource" : "arn:aws:ec2:ap-south-1:261773514547:instance/i-0cab02d22591c5fb4"
+        }
+      ]
+    }
+EOF
+}
+
 resource "aws_cloudwatch_metric_alarm" "cpu" {
     alarm_name = "cpu_alarm"
     comparison_operator = "GreaterThanOrEqualToThreshold"
